954797862fd80127711eb34f9ce55fc236e4a9e2
Matthew Cain <codingdatum@gmail.com>
2021-10-23 18:04:12 -0400

    Home pageavailable, about to configure button functionality/page display for budget pal

---
+++ src/components/Budget.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Budget = props => {
+    return (
+        <p>Budget Testing</p>
+    )
+}
+
+export default Budget;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 import styles from './HomePage.module.css'
 import HomePageButtons from './HomePageButtons';
+import Budget from './Budget';
 
 const HomePage = props => {
 
     const [weAreHome, setWeAreHome] = useState(true);
+    const [weAreBudget, setWeAreBudget] = useState(false);
 
     const renderBudgetPal = () => {
         console.log("renderBudgetPal executed")
         setWeAreHome(false)
+        setWeAreBudget(true)
     }
 
     return(
         <div className={styles["home-page"]}>
             {weAreHome && <HomePageButtons renderBudgetPal={renderBudgetPal} />}
+            {weAreBudget && <Budget />}
         </div>
     )
 }
