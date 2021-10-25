58a32befc35e3de9cbaa46b07d4387c63ddbd69b
Matthew Cain <codingdatum@gmail.com>
2021-10-24 21:23:52 -0400

    cleanup

--- src/components/EventPal/EventPal.js
+++ src/components/EventPal/EventPal.js
@@ -3,7 +3,36 @@ import styles from './EventPal.module.css';
 
 const EventPal = props => {
     return(
-        <div className={styles["event-pal"]}>FUCK YOU IT'S NOT DONE YET!!!</div>
+        <div className={styles["event-pal"]}>
+            <button>Add Event</button>
+            {/* Repeat this following several tiomes */}
+            <div className={styles["event-tabs"]}>
+                <div className={styles["event-tab"]}>
+                    <h3>today/thisweek</h3>
+                    <ul>
+                        <li>Event Info</li>
+                        <li>Event Info</li>
+                        <li>Event Info</li>
+                    </ul>
+                </div>
+                <div className={styles["event-tab"]}>
+                    <h3>today/thisweek</h3>
+                    <ul>
+                        <li>Event Info</li>
+                        <li>Event Info</li>
+                        <li>Event Info</li>
+                    </ul>
+                </div>
+                <div className={styles["event-tab"]}>
+                    <h3>today/thisweek</h3>
+                    <ul>
+                        <li>Event Info</li>
+                        <li>Event Info</li>
+                        <li>Event Info</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     )
 }
 
