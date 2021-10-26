26a3a48e4c873d11f7265dcb5778d78ff0cdbb11
Matthew Cain <codingdatum@gmail.com>
2021-10-26 13:25:46 -0400

    finished implementing a dummy list of objects and drop-down menu functionality

--- src/components/Lists/List.js
+++ src/components/Lists/List.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './List.module.css'
 
 const List = props => {
+
+    const [listIsActive, setListIsActive] = useState(false);
+
+    const listItems = props.listItems.map(item => {
+        return (
+            <li key={Math.random()}>{item}</li>
+        )
+    })
+
+    const renderThisList = () => {
+        if(listIsActive){
+            setListIsActive(false)
+        }else{
+            setListIsActive(true)
+        }
+    }
+
     return (
-        <div>testing lists</div>
+        <div className={styles.container}>
+            <div className={styles.heading}>
+                <span>➕</span>
+                <h4>{props.name}</h4>
+                <span onClick={renderThisList}>⬇</span>
+            </div>
+            <div className={styles["list-container"]}>
+                {listIsActive && <ul>{listItems}</ul>}
+            </div>
+        </div>
     )
 }
 
