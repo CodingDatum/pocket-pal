d07f5ca4ff280840c2d5c0b7546e449010e6bad2
Matthew Cain <codingdatum@gmail.com>
2021-11-01 19:21:24 -0400

    cleanup

--- src/components/Lists/List.js
+++ src/components/Lists/List.js
@@ -60,7 +60,7 @@ const List = props => {
             </div>
             <div className={styles["list-container"]}>
                 {listIsActive && <ul>{listItems}
-                    <li><button onClick={submitItemToList}>+</button><input type="text" onChange={listItemHandler}></input></li></ul>}
+                    <li><button className={styles["add-list-item-button"]} onClick={submitItemToList}>+</button><input className={styles["list-item-input"]} type="text" onChange={listItemHandler}></input></li></ul>}
             </div>
         </div>}
         </React.Fragment>
