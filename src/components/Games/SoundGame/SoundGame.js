3033fc5950864a3804f8fd605b65a6ded2b26619
Matthew Cain <codingdatum@gmail.com>
2021-11-07 14:53:09 -0500

    finisheded sounds

--- src/components/Games/SoundGame/SoundGame.js
+++ src/components/Games/SoundGame/SoundGame.js
@@ -5,20 +5,26 @@ import Button from '../../UI/Button';
 
 // import sounds
 import spookyMP3 from './sounds/spooky.mp3';
+import wooshMP3 from './sounds/Woosh.mp3';
+import homiesMP3 from './sounds/Homies.mp3';
+import dropletMP3 from './sounds/Droplet.mp3';
 
 // assign sounds:
 
 
 const SoundGame = props =>{
 
-    const [playSpooky] = useSound(spookyMP3)
+    const [playSpooky] = useSound(spookyMP3);
+    const [playWoosh] = useSound(wooshMP3);
+    const [playHomies] = useSound(homiesMP3);
+    const [playDroplet] = useSound(dropletMP3);
 
     return (
         <div className={styles["sound-game"]}> 
             <Button onClick={playSpooky} buttonName="Spooky" />
-            <Button buttonName="pending" />
-            <Button buttonName="I Forgot" />
-            <Button buttonName="Woosh?" />
+            <Button onClick={playWoosh} buttonName="Woosh" />
+            <Button onClick={playHomies} buttonName="Homies" />
+            <Button onClick={playDroplet} buttonName="Droplet" />
         </div>
     )
 }
