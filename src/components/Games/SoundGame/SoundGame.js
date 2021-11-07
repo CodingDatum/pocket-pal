95a1c0a2facce70c66a27a6912fd02ebb5b59d2a
Matthew Cain <codingdatum@gmail.com>
2021-11-07 14:25:33 -0500

    first sound!

--- src/components/Games/SoundGame/SoundGame.js
+++ src/components/Games/SoundGame/SoundGame.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import UIfx from 'uifx';
-import styles from './SoundGame.module.css'
+import useSound from 'use-sound';
+import styles from './SoundGame.module.css';
+import Button from '../../UI/Button';
 
 // import sounds
 import spookyMP3 from './sounds/spooky.mp3';
 
 // assign sounds:
-const spooky = new UIfx({asset: spookyMP3})
+
 
 const SoundGame = props =>{
 
-    const spookyHandler = () => {
+    const [playSpooky] = useSound(spookyMP3)
 
-        spooky.play()
-
-    }
     return (
-        <div className={styles.testing}> 
-            <button onClick={spookyHandler}>Spooky</button>
-            <button>Droplet</button> 
-            <button>Woosh</button> 
-            <button>Homies</button> 
+        <div className={styles["sound-game"]}> 
+            <Button onClick={playSpooky} buttonName="Spooky" />
+            <Button buttonName="pending" />
+            <Button buttonName="I Forgot" />
+            <Button buttonName="Woosh?" />
         </div>
     )
 }
