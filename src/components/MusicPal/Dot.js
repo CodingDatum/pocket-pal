c07e12de0ff40349f3eb5d4c45fb19b3d2cca195
Matthew Cain <codingdatum@gmail.com>
2021-12-23 14:59:04 -0500

    chprd functionality done for basic chords

---
+++ src/components/MusicPal/Dot.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Dot.module.css'
+
+const Dot = props => {
+    return <div className={styles.dot}></div>
+};
+
+export default Dot;
--- src/components/MusicPal/Piano.js
+++ src/components/MusicPal/Piano.js
@@ -1,31 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Dot from './Dot';
 
 import styles from './Piano.module.css';
 
 const Piano = props => {
 
+    const [currentChord, setCurrentChord] = useState(null);
+
+    const showA = () => {
+        setCurrentChord("A")
+    }
+
+    const showB = () => {
+        setCurrentChord("B")
+    }
+
+    const showC = () => {
+        setCurrentChord("C")
+    }
+
+    const showD = () => {
+        setCurrentChord("D")
+    }
+
+    const showE = () => {
+        setCurrentChord("E")
+    }
+
+    const showF = () => {
+        setCurrentChord("F")
+    }
+
+    const showG = () => {
+        setCurrentChord("G")
+    }
+
     return (
-        <div className={styles.piano}>
+        <React.Fragment>
+            <div className={styles.piano}>
             <div className={styles["key-container"]}>
                 <div className={styles["white-keys"]}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
+                    <span>{(currentChord === "C" || currentChord === "F") && <Dot />}</span>
+                    <span>{(currentChord === "D" || currentChord === "G") && <Dot />}</span>
+                    <span>{(currentChord === "E" || currentChord === "A" || currentChord === "C") && <Dot />}</span>
+                    <span>{(currentChord === "F") && <Dot />}</span>
+                    <span>{(currentChord === "G" || currentChord === "C") && <Dot />}</span>
+                    <span>{(currentChord === "A" || currentChord === "D" || currentChord === "F") && <Dot />}</span>
+                    <span>{(currentChord === "B" || currentChord === "G" || currentChord === "E") && <Dot />}</span>
                 </div>
                 <div className={styles["black-keys"]}>
-                    <span></span>
-                    <span></span>
+                    <span>{(currentChord === "A") && <Dot />}</span>
+                    <span>{(currentChord === "B") && <Dot />}</span>
                     <span className={styles.hidden}></span>
-                    <span></span>
-                    <span></span>
+                    <span>{(currentChord === "D" || currentChord === "B") && <Dot />}</span>
+                    <span>{(currentChord === "E") && <Dot />}</span>
                     <span></span>
                 </div>
             </div>
         </div>
+        <div className={styles["chord-buttons"]}>
+
+            <button onClick={showA}>A</button>
+            <button onClick={showB}>B</button>
+            <button onClick={showC}>C</button>
+            <button onClick={showD}>D</button>
+            <button onClick={showE}>E</button>
+            <button onClick={showF}>F</button>
+            <button onClick={showG}>G</button>
+            
+        </div>
+        </React.Fragment>
     )
 }
 
