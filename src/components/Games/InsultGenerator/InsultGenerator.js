798c107f7b98cc641f40d3197b665b5b711aa80d
Matthew Cain <codingdatum@gmail.com>
2022-06-03 23:17:54 -0400

    finished iplementing logic behind insult generator

--- src/components/Games/InsultGenerator/InsultGenerator.js
+++ src/components/Games/InsultGenerator/InsultGenerator.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
 import styles from './InsultGenerator.module.css';
 
+const listOne = ["Fuck-", "Cunt-", "Shit-", "Ass-", "Pussy-", "Bitch-", "Clit-", "Dick-", "Fart-", "Queef-", "Slut-"];
+const listTwo = ["face", "stick", "head", "licker", "bag", "hole", "bandit", "pipe", "breath"];
+
 const InsultGenerator = () => {
 
-    const insult = useState("Click the button for a random insult");
+    const [insult, setInsult] = useState("Click the button for a random insult");
+
+    const insultGeneratorHandler = () => {
+        const randomNumberOne = Math.floor(Math.random() * listOne.length);
+        const randomNumberTwo = Math.floor(Math.random() * listTwo.length);
+        const firstWord = listOne[randomNumberOne];
+        const secondWord = listTwo[randomNumberTwo];
+        const finalInsult = firstWord + secondWord;
+        setInsult(finalInsult);
+    }
 
     return (
         <div className={styles["insult-generator__container"]}>
             <span>{insult}</span>
-            <button>Button</button>
+            <button onClick={insultGeneratorHandler}>Button</button>
         </div>
     )
 }
