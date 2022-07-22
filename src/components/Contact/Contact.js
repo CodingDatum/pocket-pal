cd305fc04c4a7f6ff604c81745dcf7822bf0b98d
Matthew Cain <codingdatum@gmail.com>
2022-07-22 14:40:35 -0400

    Finished contact section

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import ReactDom from 'react-dom';
+import styles from "./Contact.module.css"
+
+const webAddress = "https://codingdatum.github.io/pocket-pal/";
+
+const ContactContent = props => {
+
+    const [email, setEmail] = useState("");
+
+    const inputHandler = (e) => {
+        e.preventDefault();
+        setEmail(e.target.value);
+    }
+
+    const sendEmailHandler = () => {
+        window.open('mailto:' + email +'?subject="PocketPal"&body=' + webAddress);
+    }
+
+    return(
+        <div className={styles.contact}>
+            <h1>Contact Me</h1>
+            <p>Enter your email and I'll send you this app:</p>
+            <input type="text" onChange={inputHandler}></input>
+            <button onClick={sendEmailHandler}>Send App</button>
+            <p>Would you like a custom app of your own? Would you like to learn how to code? feel free to reach out to me at codingdatum@gmail.com</p>
+            <button onClick={props.contactCloseHandler}>close</button>
+        </div>
+    )
+}
+
+const Contact = props => {
+    return (
+        <React.Fragment>
+        {ReactDom.createPortal(<ContactContent contactCloseHandler={props.contactCloseHandler} />, document.getElementById("contact-root"))}
+        </React.Fragment>
+    )
+    
+}
+
+export default Contact
--- src/components/Nav/NavCol.js
+++ src/components/Nav/NavCol.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Contact from '../Contact/Contact';
 import styles from './NavCol.module.css';
 
 const NavCol = props => {
+
+    const[contactIsOpen, setContactIsOpen] = useState(false);
+
+    const contactOpenHandler = () => {
+        setContactIsOpen(true)
+    }
+
+    const contactCloseHandler = () => {
+        setContactIsOpen(false)
+    }
 
     return(
         <div className={styles["nav-col"]}>
             <ul>
                 <li onClick={props.goHome}>Home</li>
                 <li>About</li>
-                <li>Contact</li>
+                <li onClick={contactOpenHandler}>Contact</li>
             </ul>
             <div>&#169;</div>
+            {contactIsOpen && <Contact contactCloseHandler={contactCloseHandler} />}
             
         </div>
     )
