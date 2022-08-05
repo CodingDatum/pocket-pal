9bd244a55c54b6f29d3c6c675be9c37da50fc163
Matthew Cain <codingdatum@gmail.com>
2022-08-05 19:56:59 -0400

    trying to implement insult generator so it wont repeat words, so far unsuccesfull

--- src/components/Games/InsultGenerator/InsultGenerator.js
+++ src/components/Games/InsultGenerator/InsultGenerator.js
@@ -4,22 +4,85 @@ import InsultDisclaimer from './InsultDisclaimer';
 
 const listOne = ["Fuck-", "Cunt-", "Shit-", "Ass-", "Pussy-", "Bitch-", "Clit-", "Dick-", "Fart-", "Queef-", "Slut-"];
 const listTwo = ["face", "stick", "head", "licker", "bag", "hole", "bandit", "pipe", "breath", "troll"];
-const listThree = ["Slithering", "Ratchet", "Putrid", "Decrepid", "Scaggy", "Slimey", "Unrelenting", "Ruthless", "Insepid", "Festering"]
+const listThree = ["Slithering", "Ratchet", "Putrid", "Decrepid", "Scaggy", "Slimey", "Unrelenting", "Ruthless", "Insepid", "Festering"];
+
+let completedListOne = [];
+let completedListTwo = [];
+let completedListThree = [];
+
+const updateListOne = listItem => {
+    completedListOne.push(listItem)
+    if(listOne.length === completedListOne.length){
+        completedListOne = []
+    }
+}
+const updateListTwo = listItem => {
+    completedListTwo.push(listItem)
+    if(listTwo.length === completedListTwo.length){
+        completedListTwo = []
+    }
+}
+
+const updateListThree = listItem => {
+    completedListThree.push(listItem)
+    if(listThree.length === completedListThree.length){
+        completedListThree = []
+    }
+}
+
+const createFirstWord = () => {
+    const randomNumberOne = Math.floor(Math.random() * listOne.length);
+    const wordToAdd = listOne[randomNumberOne];
+    if(completedListOne.includes(wordToAdd)){
+        createFirstWord()
+    }
+    if(!completedListOne.includes(wordToAdd)){
+        updateListOne(wordToAdd);
+    }
+
+    return(wordToAdd);
+}
+
+const createSecondWord = () => {
+    const randomNumberTwo = Math.floor(Math.random() * listTwo.length);
+    const wordToAdd = listTwo[randomNumberTwo];
+    if(completedListTwo.includes(wordToAdd)){
+        createSecondWord()
+    }
+    if(!completedListTwo.includes(wordToAdd)){
+        updateListTwo(wordToAdd);
+    }
+
+    return(wordToAdd);
+}
+
+const createThirdWord = () => {
+    const randomNumberThree = Math.floor(Math.random() * listThree.length);
+    const wordToAdd = listThree[randomNumberThree];
+    if(completedListThree.includes(wordToAdd)){
+        createThirdWord()
+    }
+    if(!completedListThree.includes(wordToAdd)){
+        updateListThree(wordToAdd);
+    }
+
+    return(wordToAdd);
+}
 
 const InsultGenerator = props => {
 
     const [insult, setInsult] = useState("Click the button for a random insult");
-    const [disclaimer, setDisclaimer] = useState(false)
+    const [disclaimer, setDisclaimer] = useState(false);
 
     const insultGeneratorHandler = () => {
-        const randomNumberOne = Math.floor(Math.random() * listOne.length);
-        const randomNumberTwo = Math.floor(Math.random() * listTwo.length);
-        const randomNumberThree = Math.floor(Math.random() * listThree.length);
-        const firstWord = listOne[randomNumberOne];
-        const secondWord = listTwo[randomNumberTwo];
-        const thirdWord = listThree[randomNumberThree];
+        const firstWord = createFirstWord();
+        const secondWord = createSecondWord();
+        const thirdWord = createThirdWord();
         const finalInsult = thirdWord + " " + firstWord + secondWord;
         setInsult(finalInsult);
+        console.log(completedListOne)
+        console.log(completedListTwo)
+        console.log(completedListThree)
     }
 
     const backButtonHandler = () => {
@@ -29,7 +92,7 @@ const InsultGenerator = props => {
     useEffect(()=>{
         setTimeout(()=>{
             setDisclaimer(true)
-        }, 2000)
+        }, 1000)
     }, [])
 
     const closeDisclaimer = () => {
